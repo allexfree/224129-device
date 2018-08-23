ed2e2ae544c7cee32388417138bb9f3fd84d886b
allefree <allexfree@yandex.ru>
2018-08-24 00:27:26 +0300

    Базовая стилизация личного проекта

--- js/common.js
+++ js/common.js
@@ -1,7 +1,19 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+
 var menuDropdown = document.querySelector('.main-menu__dropdown');
 var linksFocused = menuDropdown.querySelectorAll('.main-menu__dropdown-item a');
+
+var mapLink = document.querySelector('.contacts__map');
+var contactLink = document.querySelector('.contacts__link');
+var mapModalOverlay = document.querySelector('.map-popup-overlay');
+var formModalOverlay = document.querySelector('.form-popup-overlay');
+var inputUserName = formModalOverlay.querySelector('#name-user');
+var inputUserMail = formModalOverlay.querySelector('#mail-user');
+var formUserText = formModalOverlay.querySelector('#text-user');
+var btnCloseMap = mapModalOverlay.querySelector('.close-btn');
+var btnCloseForm = formModalOverlay.querySelector('.close-btn');
 
 for (var i = 0; i < linksFocused.length; i++) {
   linksFocused[i].onfocus = function () {
@@ -12,3 +24,31 @@ for (var i = 0; i < linksFocused.length; i++) {
     menuDropdown.setAttribute('style', '');
   }
 }
+
+var mapLinkClickHandler = function (evt) {
+  evt.preventDefault();
+  mapModalOverlay.classList.remove('hidden');
+}
+
+var contactLinkClickHandler = function (evt) {
+  evt.preventDefault();
+  formModalOverlay.classList.remove('hidden');
+}
+
+var btnCloseClickHandler = function () {
+  mapModalOverlay.classList.add('hidden');
+  formModalOverlay.classList.add('hidden');
+}
+
+var escPressHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== inputUserName && evt.target !== inputUserMail && evt.target !== formUserText) {
+    mapModalOverlay.classList.add('hidden');
+    formModalOverlay.classList.add('hidden');
+  }
+}
+
+mapLink.addEventListener('click', mapLinkClickHandler);
+contactLink.addEventListener('click', contactLinkClickHandler);
+btnCloseMap.addEventListener('click', btnCloseClickHandler);
+btnCloseForm.addEventListener('click', btnCloseClickHandler);
+document.addEventListener('keydown', escPressHandler);
